c3bcc436fed582fe81d81fff19315c279c73b572
leoo-c1 <chinchillaleo01@gmail.com>
2026-02-13 19:17:22 +1100

    Use indexed part select for assigning bits to byte

--- rtl/eth/rmii_handler.sv
+++ rtl/eth/rmii_handler.sv
@@ -12,7 +12,7 @@ module rmii_handler (
     );
 
     parameter max_bit_count = 8;        // We want to count a total of 8 bits
-    logic [1:0] bit_count = 0;          // Counts how many 2-bits we have received
+    logic [3:0] bit_counter = 0;        // Counts how many bits we have received
 
     logic [7:0] bit_storage;            // Holds onto each received bit until full byte is received
 
@@ -20,36 +20,22 @@ module rmii_handler (
         if (!resetn) begin              // On reset/startup, reset our collected byte
             received_byte <= 8'b0;
             byte_valid <= 8'b0;
-            bit_count <= 2'b0;
+            bit_counter <= 2'b0;
             bit_storage <= 8'b0;
         end else begin
             if (data_valid) begin       // If we are receiving valid data
-                // If we are about to receive the first two bits for a new byte
-                if (bit_count == 0) begin
-                    bit_storage[1:0] <= {rx1, rx0};
-                    bit_count <= bit_count + 1;
+                if (bit_counter < max_bit_count - 2) begin
+                    bit_storage[bit_counter +: 2] <= {rx1, rx0};
+                    bit_counter <= bit_counter + 2;
                     byte_valid <= 1'b0;
-                // If we are about to receive the 3rd and 4th bits of our byte
-                end else if (bit_count == 1) begin
-                    bit_storage[3:2] <= {rx1, rx0};
-                    bit_count <= bit_count + 1;
-                    byte_valid <= 1'b0;
-                // If we are about to receive the 5th and 6th bits of our byte
-                end else if (bit_count == 2) begin
-                    bit_storage[5:4] <= {rx1, rx0};
-                    bit_count <= bit_count + 1;
-                    byte_valid <= 1'b0;
-                // If we are about to receive the 7th and 8th bits of our byte
-                end else if (bit_count == 3) begin
+                end else begin
                     received_byte <= {rx1, rx0, bit_storage[5:0]};
                     byte_valid <= 1'b1;
-                    bit_count <= 0;
-                end else begin
-                    bit_count <= 0;
-                    byte_valid <= 1'b0;
+                    bit_counter <= 0;
                 end
+
             end else begin              // If we aren't receiving valid data
-                bit_count <= 0;
+                bit_counter <= 0;
                 byte_valid <= 1'b0;
             end
         end

3306651d638111bf2983d2f7b58145f9b48f1f56
leoo-c1 <chinchillaleo01@gmail.com>
2026-02-13 11:05:06 +1100

    Added package for ethernet packed struct typedefs

---
+++ rtl/pkg/eth_types_pkg.sv
@@ -0,0 +1,32 @@
+package eth_types_pkg
+
+    typedef struct packed {
+        byte unsigned dest_mac [6];
+        byte unsigned src_mac [6];
+        byte unsigned ethertype [2];
+    } frame_header;
+
+    typedef struct packed {
+        bit [3:0] version;
+        bit [3:0] header_len;
+        bit [5:0] dscp;
+        bit [1:0] ecn;
+        byte unsigned total_len [2];
+        byte unsigned identification [2];
+        bit [2:0] flags;
+        bit [12:0] frag_offset;
+        byte unsigned ttl [1];
+        byte unsigned protocol [1];
+        byte unsigned header_csum [2];
+        byte unsigned source_ip [4];
+        byte unsigned dest_ip [4];
+    } ip_header;
+
+    typedef struct packed {
+        byte unsigned src_port [2];
+        byte unsigned dest_port [2];
+        byte unsigned udp_len [2];
+        byte unsigned udp_csum [2];
+    } udp_header;
+
+endpackage

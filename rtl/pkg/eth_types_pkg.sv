36a866fb879a27e6b097636b7f3224e9e7911c34
leoo-c1 <chinchillaleo01@gmail.com>
2026-02-13 16:02:45 +1100

    Add enum type for transmission states

--- rtl/pkg/eth_types_pkg.sv
+++ rtl/pkg/eth_types_pkg.sv
@@ -1,11 +1,13 @@
 package eth_types_pkg
 
+    // Ethernet frame header components
     typedef struct packed {
         byte unsigned dest_mac [6];
         byte unsigned src_mac [6];
         byte unsigned ethertype [2];
     } frame_header;
 
+    // IP packet header components
     typedef struct packed {
         bit [3:0] version;
         bit [3:0] header_len;
@@ -18,15 +20,27 @@ package eth_types_pkg
         byte unsigned ttl [1];
         byte unsigned protocol [1];
         byte unsigned header_csum [2];
-        byte unsigned source_ip [4];
+        byte unsigned src_ip [4];
         byte unsigned dest_ip [4];
     } ip_header;
 
+    // UDP datagram header components
     typedef struct packed {
         byte unsigned src_port [2];
         byte unsigned dest_port [2];
         byte unsigned udp_len [2];
         byte unsigned udp_csum [2];
     } udp_header;
+
+    typedef enum {
+        IDLE,
+        PREAMBLE_SFD,  // Waiting for 0xD5
+        ETH_HEADER,    // Reading MACs and EtherType
+        IP_HEADER,     // Reading IP header
+        UDP_HEADER,    // Reading UDP header
+        PAYLOAD,       // Reading the payload data
+        FCS,           // Checking the CRC
+        DONE           // Sends a pulse for valid output
+    } eth_states;
 
 endpackage

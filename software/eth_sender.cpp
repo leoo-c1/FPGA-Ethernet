33c9ce9d363fb2b6bac40262f674366e11cfe106
leoo-c1 <chinchillaleo01@gmail.com>
2026-02-16 22:50:36 +1100

    Send test message to FPGA

--- software/eth_sender.cpp
+++ software/eth_sender.cpp
@@ -1,6 +1,7 @@
 #include <winsock2.h>
 #include <iostream>
 #include <ws2tcpip.h>
+#include <string>
 
 int main() {
     WSADATA wsadata;
@@ -28,7 +29,18 @@ int main() {
             inet_pton(AF_INET, "192.0.2.146", &server_addr.sin_addr);
 
             std::cout << "Address struct created." << std::endl;
-            
+
+            std::string message = "FPGA test message";
+
+            int send_result = sendto(data_socket, message.c_str(), message.length(), 0, (sockaddr*)&server_addr, sizeof(server_addr));
+
+            if (send_result == SOCKET_ERROR) {
+                std::cout << "Send failed, error code: " << WSAGetLastError() << std::endl;
+            }
+
+            else {
+                std::cout << "Sent " << send_result << " bytes" << std::endl;
+            }
 
             closesocket(data_socket);
         }
